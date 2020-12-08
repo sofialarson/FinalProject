3d2767af7fcc428497ade770880a82167d51d2fa
Your Name <srl485@nyu.edu>
2020-12-07 20:02:31 -0800

    files

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 //webcam code referenced  from https://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm  //
 
-var video = document.querySelector(".webcam");
+var video = document.querySelector("#videoElement");
 
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia( {video: true})
-  .then(function(stream) {
-    video.srcObject = stream;
-  })
+  navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function (stream) {
+      video.srcObject = stream;
+    })
+    .catch(function (err0r) {
+      console.log("Something went wrong!");
+    });
 }
